158ce051a3e0de39e5d7257c2fcf612b1062e1d0
juan jose <juanjosenarvaezmarin13092005@gmail.com>
2026-03-14 09:57:24 -0500

    proyecto spring boot maven

---
+++ src/main/java/com/descuentos/descuentos_mio/DescuentosMioApplication.java
@@ -0,0 +1,13 @@
+package com.descuentos.descuentos_mio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescuentosMioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescuentosMioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/descuentos/descuentos_mio/DescuentosMioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.descuentos.descuentos_mio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescuentosMioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
