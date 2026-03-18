5c40b4463b325845707675a36678bdd5f52daf55
Jean Paul <jeanpaulortizrestrepo1@gmail.com>
2026-03-18 00:28:49 -0500

    se avanza en el modulo de recibos de caja y se agrega documentacion API (Swagger)

--- src/main/java/com/co/eatupapi/EatupapiApplication.java
+++ src/main/java/com/co/eatupapi/EatupapiApplication.java
@@ -2,8 +2,18 @@ package com.co.eatupapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+
+//Ignora la libreria temporalmente de jpa y postgres, para evitar error en la compitalacion
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
+
 public class EatupapiApplication {
 
 	public static void main(String[] args) {
